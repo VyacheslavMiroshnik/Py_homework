b5214e09a116d7a9284c55a58617ee080ff2dfd8
Vyacheslav <mepa087@gmail.com>
2020-04-09 12:45:18 +0300

    create 1 method for search words

--- hm10/read_json_xml.py
+++ hm10/read_json_xml.py
@@ -3,25 +3,31 @@ from pprint import pprint
 from datetime import datetime
 import xml.etree.ElementTree as ET
 class json_top():
-    def json_read(self):
-        self.file_dic = json.load(self.file)
-        serch_list = self.file_dic["rss"]["channel"]["items"]
+
+    def top_words(self,some_list):
         temp_list = []
-        tem_dic = {}    
-        for item in serch_list:
-            temp_list = (list(filter(lambda x: len(x)>=10,item["description"].split())))
+        temp_dic = {} 
+        for item in some_list:
+            temp_list = list(filter(lambda x: len(x)>=6, item))
             for word in temp_list:
                 word = word.lower()
-                if word not in tem_dic:
-                    tem_dic[word] = 1
+                if word not in temp_dic:
+                    temp_dic[word] = 1
                 else :
-                    tem_dic[word] += 1    
-            
-        return list(sorted(tem_dic.items(),key=lambda item: -item[1]))[:10]
-        
-    def xml_read(self):
+                    temp_dic[word] += 1  
+        return temp_dic              
+    def json_read(self):
+
         temp_list = []
-        tem_dic = {}   
+        self.file_dic = json.load(self.file)
+        list_items = self.file_dic["rss"]["channel"]["items"]
+        for item in list_items:
+            temp_list.append(item["description"].split())
+        temp_dic = self.top_words(temp_list)    
+        return list(sorted(temp_dic.items(),key = lambda item: -item[1]))[:10]
+        
+    def xml_read(self): 
+        temp_list = []
         parser = ET.XMLParser(encoding = "utf-8")
         tree = ET.parse(self.file, parser)    
         root = tree.getroot()
@@ -29,14 +35,10 @@ class json_top():
         items = channel.findall("item")
         for item in items:
             description = item.find("description")
-            temp_list = (list(filter(lambda x: len(x)>=10,description.text.split())))
-            for word in temp_list:
-                word = word.lower()
-                if word not in tem_dic:
-                    tem_dic[word] = 1
-                else :
-                    tem_dic[word] += 1    
-        return list(sorted(tem_dic.items(),key = lambda item: -item[1]))[:10]    
+            temp_list.append(description.text.split())
+        temp_dic = self.top_words(temp_list)
+
+        return list(sorted(temp_dic.items(),key = lambda item: -item[1]))[:10]    
 
 
     def error_log(self, exc_type, exc_val, exc_tb):
